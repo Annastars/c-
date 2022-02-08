522f7cd27813709118cfb782425a07c57affe47f
Anna Kyryllova <hannakyryllova@gmail.com>
2022-02-08 17:01:10 +0100

    if-else Marie

---
+++ test_task05_ifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("add username: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "marie")
+{
+	Console.WriteLine("Yahoo, This is MARIE!");
+}
+else
+{
+	Console.Write("Hello, ");
+	Console.WriteLine(username);
+}
