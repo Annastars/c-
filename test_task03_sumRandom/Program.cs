7280121ac053d81683d6c5d750418cba8ac759b0
Anna Kyryllova <hannakyryllova@gmail.com>
2022-02-12 12:00:58 +0100

    new Random

---
+++ test_task03_sumRandom/Program.cs
@@ -0,0 +1,7 @@
+﻿int numbera = new Random().Next(1, 9); // 1 2 3 4 5 ... 9
+Console.WriteLine(numbera);
+int numberb = new Random().Next(1, 9);
+Console.WriteLine(numberb);
+int result = numbera + numberb;
+Console.WriteLine(result);
+//Console.WriteLine(numberA + numberb);
